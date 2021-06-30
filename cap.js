b91834d2c9371783627e98091a57c367e3b230dc
Alexander Williams <alex.williams2311@gmail.com>
2021-06-29 22:35:29 -0700

    lab 12

---
+++ cap.js
@@ -0,0 +1,20 @@
+'use strict';
+// the Main Hub App
+
+const events = require('./event-pool.js');
+
+events.on('vendor', () => {
+  //simulate customer order
+
+  //Declare store name
+
+  //create stroe order as object - storeName, orderId, customerName, address
+
+  //emit pickup event and attach fake order as payload
+
+  //monitor for delivered events and consoleLog "thank you"
+})
+
+
+
+//extned
